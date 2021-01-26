22a22908cf889e5e5b2f2822a076ede97ee3b4ad
Hai Tran <hai@bio-rithm.com>
2021-01-26 22:08:07 +0700

    First commit

---
+++ SwiftUIBLE/ContentView.swift
@@ -0,0 +1,92 @@
+//
+//  ContentView.swift
+//  SwiftUIBLE
+//
+//  Created by hai on 26/1/21.
+//  Copyright © 2021 biorithm. All rights reserved.
+//
+
+import SwiftUI
+import CoreBluetooth
+
+struct Peripheral: Identifiable {
+    let id: Int
+    let name: String
+    let rssi: Int
+    let uuid: String
+}
+
+class BLEManager: NSObject, ObservableObject, CBCentralManagerDelegate {
+    @Published var peripherals = [Peripheral]()
+    @Published var isSwitchedOn = false
+    @Published var isScanning = false
+    
+    var myCentral: CBCentralManager!
+    override init() {
+        super.init()
+        myCentral = CBCentralManager(delegate: self, queue: nil)
+        myCentral.delegate = self
+    }
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        if central.state == .poweredOn {
+            isSwitchedOn = true
+            print("BLE power on")
+        }
+        else {
+            isSwitchedOn = false
+            print("BLE power off")
+        }
+    }
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
+        var peripheralName: String!
+        if let name = advertisementData[CBAdvertisementDataLocalNameKey] as? String {
+            peripheralName = name
+        }
+        else {
+            peripheralName = "Unknown"
+        }
+        
+        let newPeripheral = Peripheral(id: peripherals.count,
+                                       name: peripheralName,
+                                       rssi: RSSI.intValue,
+                                       uuid: peripheral.identifier.uuidString)
+        print(newPeripheral)
+        peripherals.append(newPeripheral)
+    }
+    func scan(){
+        print("start scanning ")
+        self.isScanning = true
+        myCentral.scanForPeripherals(withServices: nil, options: nil)
+    }
+    func stopScanning() {
+        print("stopScanning")
+        self.isScanning = false
+        self.myCentral.stopScan()
+    }
+}
+
+struct BLEPeripheralTableView : View {
+    @ObservedObject var sot = BLEManager()
+    var body: some View {
+        NavigationView{
+            List(self.sot.peripherals){device in
+                Text("uuid:\(String(device.uuid.prefix(4)))-name:\(device.name)-rssi:\(device.rssi)")
+                .lineLimit(1)
+            }
+            .navigationBarTitle(Text("BLE"))
+            .navigationBarItems(trailing: Button(action: {self.scanBLEDevices()}){
+                Text(self.sot.isScanning ? "stop" : "scan" )
+            })
+        }
+    }
+    
+    func scanBLEDevices(){
+        self.sot.isScanning ? self.sot.stopScanning() : self.sot.scan()
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+        BLEPeripheralTableView()
+    }
+}
